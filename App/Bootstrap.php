85cdf58b16f7488bbb73dce0962ef832c70cef31
Alex MacKenzie <alexbcmac7@gmail.com>
2018-09-05 18:51:15 -0700

    Add routing and home controller

---
+++ App/Bootstrap.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types = 1);
+
+namespace App;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+// Register the error handler
+$whoops = new \Whoops\Run;
+
+if ($environment !== 'production') {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function($e){
+        echo 'Todo: Friendly error page and send an email to the developer';
+    });
+}
+
+$whoops->register();
+
+$routeDefinitionCallback = function (\FastRoute\RouteCollector $r) {
+    $routes = include('Routes.php');
+    foreach ($routes as $route) {
+        $r->addRoute($route[0], $route[1], $route[2]);
+    }
+};
+
+$dispatcher = \FastRoute\simpleDispatcher($routeDefinitionCallback);
+
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        print_r('not allowed');
+        // ... 405 Method Not Allowed
+        break;
+    case \FastRoute\Dispatcher::FOUND:
+    $className = $routeInfo[1][0];
+    $method = $routeInfo[1][1];
+    $vars = $routeInfo[2];
+    
+    $class = new $className;
+    $class->$method($vars);
+    break;
+}
--- public/index.php
+++ public/index.php
@@ -1,30 +1,4 @@
-<? php
-
+<?php
 declare(strict_types = 1);
 
 require __DIR__ . '/../App/Bootstrap.php';
-
-
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <link rel="stylesheet" href="css/styles.css">
-//     <!-- todo -->
-
-//     <title>Invoice Creation</title>
-// </head>
-
-// <body>
-
-// <div class="container">
-//     Hello World
-// </div>
-
-// <script src="./js/scripts.js"></script> 
-
-// </body>
-
-// </html>
